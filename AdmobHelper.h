c5969f80641984ab5c0c613c98100cdb5014c8e6
mudafar <mudafar@ula.ve>
2014-04-08 17:39:08 -0430

    Create AdmobHelper.h

---
+++ AdmobHelper.h
@@ -0,0 +1,21 @@
+/*
+Copyright (c) 2014 Mudafar
+GPLv3
+*/
+
+
+#ifndef  __ADMOB_HELPER_H_
+#define  __ADMOB_HELPER_H_
+
+class AdmobHelper
+{
+public:
+	static void hideAd();
+	static void showAd();
+	static bool isAdShowing;
+
+};
+
+
+#endif // __ADMOB_HELPER_H_
+
